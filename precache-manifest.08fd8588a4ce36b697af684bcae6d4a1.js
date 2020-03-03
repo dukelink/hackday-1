ed5494d04d6be9ac227a4c3cb8f44666706ed73b
dukelink <duke.lotherington@gmail.com>
2020-03-02 18:18:57 -0800

    push frontend build folder to gh pages

---
+++ precache-manifest.08fd8588a4ce36b697af684bcae6d4a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649c5098a80cdddcca274353e36e69f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5578b5ec75e0a1a44e4",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "ad7571db4c77b4808c74",
+    "url": "/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "a5578b5ec75e0a1a44e4",
+    "url": "/static/js/2.f6b8d575.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f6b8d575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad7571db4c77b4808c74",
+    "url": "/static/js/main.94f27e12.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
